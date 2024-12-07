308213653444fc1d6f58079fe5af360dfd0aef8c
tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2024-12-07 16:27:25 +0100

    wywolanie do api w CountryService

--- src/main/java/com/example/countryapp/service/CountryService.java
+++ src/main/java/com/example/countryapp/service/CountryService.java
@@ -1,8 +1,14 @@
 package com.example.countryapp.service;
 
 import com.example.countryapp.model.Country;
+import com.example.countryapp.repository.CountryRepository;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.core.ParameterizedTypeReference;
 
 import java.util.List;
 
@@ -22,5 +28,45 @@ public class CountryService {
         return countryRepository.findByAlpha3Code(alpha3Code).orElseGet(() -> fetchAndSaveCountry(alpha3Code));
     }
 
+    private Country fetchAndSaveCountry(String alpha3Code) {
+        String apiUrl = "https://restcountries.com/v3.1/alpha/" + alpha3Code;
+
+        // Wywołanie do API i deserializacja odpowiedzi jako lista
+        ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+
+        List<Map<String, Object>> countries = response.getBody();
+        if (countries != null && !countries.isEmpty()) {
+            Map<String, Object> countryData = countries.get(0);
+            Country country = new Country();
+
+            // Mapowanie danych z API na obiekt Country
+            country.setAlpha3Code((String) countryData.get("cioc"));
+
+            Map<String, Object> name = (Map<String, Object>) countryData.get("name");
+            country.setNameCommon((String) name.get("common"));
+            country.setNameOfficial((String) name.get("official"));
+
+            country.setCapital((List<String>) countryData.get("capital"));
+            country.setRegion((String) countryData.get("region"));
+            country.setSubregion((String) countryData.get("subregion"));
+            country.setPopulation((Integer) countryData.get("population"));
+            country.setLanguages((List<String>) countryData.get("languages"));
+            country.setBorders((List<String>) countryData.get("borders"));
+            country.setTimezones((List<String>) countryData.get("timezones"));
+            country.setCurrencies((List<String>) countryData.get("currencies"));
+
+            return country;
+        } else {
+            throw new RuntimeException("No country data found for alpha3Code: " + alpha3Code);
+        }
+    }
+
     // to do ..
+
 }
