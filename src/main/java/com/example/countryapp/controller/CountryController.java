f4af087230ba2497c1535b26392bb5266b94a76b
tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2024-12-07 17:41:41 +0100

    CountryController definition

---
+++ src/main/java/com/example/countryapp/controller/CountryController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Country;
+import com.example.demo.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    private final CountryService countryService;
+
+    @Autowired
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/{alpha3Code}")
+    public Country getCountry(@PathVariable String alpha3Code) {
+        return countryService.getCountryByAlpha3Code(alpha3Code);
+    }
+}
